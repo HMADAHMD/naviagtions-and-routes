b09115e5e274c06aff17f8cca2dd8f6b45dd5030
HAMMAD AHMAD <hammadja01@gmail.com>
2023-01-30 16:35:07 +0500

    Initiate

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'SecondScreen.dart';
+
+void main() {
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const SecondScreen();
+          }));
+        },
+        child: Text("Go to Screen two"),
+      )),
+    );
+  }
+}
